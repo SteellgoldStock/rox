5a1efb14cad331527d4bf195919e4301eca730af
UnknowG <unknowgdev@gmail.com>
2020-07-22 00:25:52 +0200

    Add translation message

--- database/lang/en.js
+++ database/lang/en.js
@@ -9,16 +9,21 @@ const languageData = {
     NOT_PRENIUM: `You are not a member of Rox prenium, so you cannot activate a server.`,
     ALREADY_ACTIVED: `This server has already been activated.`,
     ACTIVE_USED: `You have already actived a server`,
+    MISSED_ARGUMENTS: `You have missed arguments`,
 
     // HELP MSG
     ROLES_TYPE_HELP: (prefix) => `You forgot the argument \`type\` here is a help to better explain how the command works: \`${prefix}roles [adminRole/modRole] [Role name]\``,
     ROLES_ROLE_HELP: (prefix) => `You forgot the argument \`role\` here is a help to better explain how the command works: \`${prefix}roles [adminRole/modRole] [Role name]\``,
     ROLE_NOT_EXIST: (role) => `The role \`${role}\` is not exist`,
     ROLES_TYPE_LIST: (action) => `The type \`${action}\` not exist, use \`adminRole\` or \`modRole\``,
-    ROLES_UPDATED: `Roles has been modified`,
+
+    EMBED_CONF_OPTION: `You have missed the argument \`option\` here is the list of options avaible, \`[config/status]\``,
+    EMBED_CONF_OPTION_ARGS: `You have missed the argument \`param\` here is the list of parameters avaible, \`[title/imgUrl]\``,
+    EMBED_CONF_OPTION_STATUS: `You have missed the argument \`status\` here is the list of status avaible, \`[on/off]\``,
 
     // SUCCESS MSG
-    ACTIVED_SERVER: (serverName, prefix) => `You have been activated the server ${serverName} execute the \`${prefix}benefits\` to see the advantages of prenium`
+    ACTIVED_SERVER: (serverName, prefix) => `You have been activated the server ${serverName} execute the \`${prefix}benefits\` to see the advantages of prenium`,
+    UPDATED: `Your changes have been saved`
 };
 
 const translate = (key, ...args) => {
--- database/lang/fr.js
+++ database/lang/fr.js
@@ -9,16 +9,21 @@ const languageData = {
     NOT_PRENIUM: `Vous n'êtes pas membre de Rox Prenium, vous ne pouvez donc pas activer un serveur`,
     ALREADY_ACTIVED: `Ce serveur a déjà été activé`,
     ACTIVE_USED: `Vous avez déjà activé un serveur`,
+    MISSED_ARGUMENTS: `Vous avez manqué des arguments`,
+
+    EMBED_CONF_OPTION: `Vous avez raté l'argument \`option\` voici la liste des options disponibles, \`[config/status]\``,
+    EMBED_CONF_OPTION_ARGS: `Vous avez raté l'argument \`param\` voici la liste des paramètres disponibles, \`[title/imgUrl]\``,
+    EMBED_CONF_OPTION_STATUS: `Vous avez raté l'argument \`status\` voici la liste des statuts disponibles, \`[on/off]\``,
 
     // HELP MSG
     ROLES_TYPE_HELP: (prefix) => `Vous avez oublié l'argument \`type\` voici une aide pour mieux vous expliquer comment fonctione la commande: \`${prefix}roles [adminRole/modRole] [Nom du Rôle]\``,
     ROLES_ROLE_HELP: (prefix) => `Vous avez oublié l'argument \`role\` voici une aide pour mieux vous expliquer comment fonctione la commande: \`${prefix}roles [adminRole/modRole] [Nom du Rôle]\``,
     ROLE_NOT_EXIST: (role) => `Le rôle \`${role}\` n'existe pas`,
     ROLES_TYPE_LIST: (action) => `Le type \`${action}\` n'existe pas, utilisez plutôt \`adminRole\` ou \`modRole\``,
-    ROLES_UPDATED: `Les rôles ont été modifiés`,
 
     // SUCCESS MSG
-    ACTIVED_SERVER: (serverName) => `Vous avez été activé le serveur ${serverName} exécuter la commande \`${prefix}benefits\` pour voir les avantages de ce serveur grâce au prenium`
+    ACTIVED_SERVER: (serverName) => `Vous avez été activé le serveur ${serverName} exécuter la commande \`${prefix}benefits\` pour voir les avantages de ce serveur grâce au prenium`,
+    UPDATED: `Your changes have been saved`
 };
 
 const translate = (key, ...args) => {
