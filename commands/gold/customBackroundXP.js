eceabfbe7ac61926dbcf845ccd54499ad0b1cc99
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2020-07-24 21:26:14 +0200

    Create customBackroundXP.js

---
+++ commands/gold/customBackroundXP.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const { client, botConfg, fs, colors,messages} = require("../../rox");
+
+module.exports.run = async (client, message, args, fs, botConfg, colors, db, dbC, dbXp, language) => {
+    let dbu = JSON.parse(fs.readFileSync("database/users/users.json", "utf8"));
+    let user = dbu[message.author.id];
+    if(!user.prenium == true) return await messages.sendMsg(message,message.guild.id,language("NOT_PRENIUM"),message.guild.name);
+
+    let image = new Image();
+
+    image.src = args;
+
+}
+
+exports.help = {
+    name: "background",
+    type: "gold"
+}
