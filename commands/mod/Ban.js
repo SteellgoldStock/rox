3b255f50183e082d96b9bb14372ec3ebace5e63e
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2020-07-24 16:55:58 +0200

    Update Ban.js

--- commands/mod/Ban.js
+++ commands/mod/Ban.js
@@ -3,6 +3,16 @@ const { client, botConfg, fs, colors,messages} = require("../../rox");
 
 module.exports.run = async (client, message, args, fs, botConfg, colors, db, dbC, dbXp, language) => {
 
+let banned = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(permission);
+if(!banned) return message.channel.send("𝐂𝐞 𝐣𝐨𝐮𝐞𝐮𝐫 𝐧'𝐞𝐱𝐢𝐬𝐭𝐞 𝐩𝐚𝐬 !");
+if(!args[0]) return message.channel.send(prefix + '**ban** <𝐦𝐞𝐧𝐭𝐢𝐨𝐧> <𝐫𝐚𝐢𝐬𝐨𝐧>');
+
+let reason = args.slice(1,100);
+
+message.guild.member(banned).ban(reason);
+
 }
 
 exports.help = {
