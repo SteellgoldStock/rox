01b6c7f8113ea9969ee48d9b7f6bc4b0e69932e8
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2020-07-24 22:28:12 +0200

    Update lovecalc.js

--- commands/fun/lovecalc.js
+++ commands/fun/lovecalc.js
@@ -1,1 +1,12 @@
+const Discord = require("discord.js");
+const { client, botConfg, fs, colors,messages} = require("../../rox");
 
+module.exports.run = async (client, message, args, fs, botConfg, colors, db, dbC, dbXp, language) => {
+
+
+}
+
+exports.help = {
+    name: "lovecalc",
+    type: "fun"
+}
