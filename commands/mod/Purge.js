675011e4cb949ad9c58a8e529c61db4f4af7930b
UnknowG <unknowgdev@gmail.com>
2020-07-24 17:38:51 +0200

    Update type

--- commands/mod/Purge.js
+++ commands/mod/Purge.js
@@ -1,1 +1,11 @@
+const Discord = require("discord.js");
+const { client, botConfg, fs, colors,messages} = require("../../rox");
 
+module.exports.run = async (client, message, args, fs, botConfg, colors, db, dbC, dbXp, language) => {
+
+}
+
+exports.help = {
+    name: "purge",
+    type: "mod"
+}
