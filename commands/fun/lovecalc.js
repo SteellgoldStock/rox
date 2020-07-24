1d7a279c70bd6eb4242cdf6cffee0217bad3c74f
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2020-07-24 22:29:37 +0200

    Update lovecalc.js

--- commands/fun/lovecalc.js
+++ commands/fun/lovecalc.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { client, botConfg, fs, colors,messages} = require("../../rox");
+const { Canvas } = require('canvas-constructor');
 
 module.exports.run = async (client, message, args, fs, botConfg, colors, db, dbC, dbXp, language) => {
 
