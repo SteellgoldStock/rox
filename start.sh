b895a51ed52504cd2dcb0bd8373f96e1eb6de489
UnknowG <unknowgdev@gmail.com>
2020-07-27 17:31:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,15 +9,8 @@ finish()
 trap finish SIGINT
 
 while (( running )); do
-    cd ../
-    rm -rf Rox/
-    mkdir Rox
-    cd Rox
-    sudo git clone ROx:UnknowG/Rox.git
-    sleep 3
-    echo "gaetan09hus"
-    EOF
     node rox.js
     echo "Restarting server on crash.."
     sleep 5
 done
+# sudo git clone ROx:UnknowG/Rox.git
