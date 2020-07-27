73c956269a339eb37c6c2c6755bbe8f067dff9d8
UnknowG <unknowgdev@gmail.com>
2020-07-27 17:33:40 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,7 @@
 clear
 
-while (( running )); do
-  cd ../
-  rm -rf Rox/
-  mkdir Rox
-  cd Rox
-  print sudo git clone ROx:UnknowG/Rox.git
-done
+cd ../
+rm -rf Rox/
+mkdir Rox
+cd Rox
+print sudo git clone ROx:UnknowG/Rox.git
