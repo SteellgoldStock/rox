e60b2bffd16ec0f2a8251a428ec3a01202a3cfa3
UnknowG <unknowgdev@gmail.com>
2020-07-28 19:08:35 +0200

    A faire

--- events/member/message.js
+++ events/member/message.js
@@ -15,23 +15,16 @@ client.on("message", message => {
 
         const language = require('../../database/lang/' + dataServer.lang)
 
-
         if (message.content.indexOf(prefix) !== 0) return;
 
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
-        if (message.guild.id == 699343389291839900){
-            return message.reply(":warning: Le bot est actuellement en développement, si vous voulez suivre l'aventure rejoingez le discord du bot, l'invitation sera donnée lors ce que le bot sera finalisée ou prêt à être en bêta\n> https://discord.gg/Yee8dWA").then(message => {
-                message.delete({ timeout: 5000 })
-            })
-        }
-
         database.query(`SELECT * FROM blacklist WHERE userid=${message.author.id}`, function (error, results, fields) {
             if (error) {
                 return false;
             } else if (results.length > 0) {
-                return message.channel.send("Vous êtes blacklist dut bot CHEH")
+                return message.channel.send("Vous êtes blacklist du bot CHEH") // Message a edit via en.js 
             } else {
                 const cmd = client.commands.get(command);
                 if (!cmd) return;
