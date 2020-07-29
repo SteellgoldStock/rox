240209ab164df543482103464cd3efbea7f5cfef
morcheysha77 <laurentmorgan77@gmail.com>
2020-07-29 09:47:45 +0100

    Test

--- commands/mod/Ban.js
+++ commands/mod/Ban.js
@@ -7,24 +7,52 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
     const reason = args.slice(1).join(" ");
     const member = message.guild.member(user);
 
-    if (user) return await msg.sendMsg("PU_NO_MENTION", message, dataServer);
-    if (member) return await msg.sendMsg("PU_NO_USER", message, dataServer);
-    if (member.user.id !== message.author.id) return await msg.sendMsg("PUNISH_Y", message, dataServer);
-    if (!reason){
+    if (user){
 
-        const reason = "No reason";
+        if (member){
+
+           if (member.user.id !== message.author.id){
+
+               if (!reason){
+
+                   const reason = "No reason";
+
+               }
+
+               if(message.member.roles.cache.has(dataServer.modRole) && (!message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.modRole) || !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) || message.member.roles.cache.has(dataServer.adminRole) && !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)){
+
+                   member.ban({ reason: reason})
+                       .then(() => {
+                           return msg.sendMsgA(language("SUCCESS_BAN", message.author.username, member.user.username, reason), message, dataServer)
+                       })
+                       .catch(err => {
+                           msg.sendMsg("PU_IMPOSSIBLE", message, dataServer);
+                           return console.error(err);
+                       });
+
+               } else {
+
+                   return await msg.sendMsg("PERMISSION_DENIED", message, dataServer);
+
+               }
+
+           } else {
+
+               return await msg.sendMsg("PUNISH_Y", message, dataServer);
+
+           }
+
+        } else {
+
+            return await msg.sendMsg("PU_NO_USER", message, dataServer);
+
+        }
+
+    } else {
+
+        return await msg.sendMsg("PU_NO_MENTION", message, dataServer);
 
     }
-    if(message.member.roles.cache.has(dataServer.modRole) && (!message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.modRole) || !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) || message.member.roles.cache.has(dataServer.adminRole) && !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) return await msg.sendMsg("PERMISSION_DENIED", message, dataServer);
-
-     member.ban({ reason: reason})
-                        .then(() => {
-                            return msg.sendMsgA(language("SUCCESS_BAN", message.author.username, member.user.username, reason), message, dataServer)
-                        })
-                        .catch(err => {
-                            msg.sendMsg("PU_IMPOSSIBLE", message, dataServer);
-                            return console.error(err);
-                        });
 }
 
 exports.help = {
