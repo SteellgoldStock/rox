86f9511807c4e600fa9e8f16e28c8a357c8e37c8
morcheysha77 <laurentmorgan77@gmail.com>
2020-07-29 09:40:17 +0100

    Test

--- commands/mod/Ban.js
+++ commands/mod/Ban.js
@@ -2,39 +2,29 @@ const Discord = require("discord.js");
 const { client, botConfg, fs, colors,msg} = require("../../rox");
 
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
-    if(message.member.roles.cache.has(dataServer.modRole) && (!message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.modRole) || !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) || message.member.roles.cache.has(dataServer.adminRole) && !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) return await msg.sendMsg("PERMISSION_DENIED", message, dataServer);
 
     const user = message.mentions.users.first();
     const reason = args.slice(1).join(" ");
-    if (!reason) {
-        const reason = "No reason"
+    const member = message.guild.member(user);
+
+    if (user) return await msg.sendMsg("PU_NO_MENTION", message, dataServer);
+    if (member) return await msg.sendMsg("PU_NO_USER", message, dataServer);
+    if (member.user.id !== message.author.id) return await msg.sendMsg("PUNISH_Y", message, dataServer);
+    if (!reason){
+
+        const reason = "No reason";
+
     }
+    if(message.member.roles.cache.has(dataServer.modRole) && (!message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.modRole) || !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) || message.member.roles.cache.has(dataServer.adminRole) && !message.guild.member(message.mentions.users.first()).roles.cache.has(dataServer.adminRole)) return await msg.sendMsg("PERMISSION_DENIED", message, dataServer);
 
-    if (user) {
-        const member = message.guild.member(user);
-
-        if (member) {
-            if (member.user.id !== message.author.id) {
-                    member
-                        .ban({
-                            reason: reason,
-                        })
+     member.ban({ reason: reason})
                         .then(() => {
                             return msg.sendMsgA(language("SUCCESS_BAN", message.author.username, member.user.username, reason), message, dataServer)
                         })
                         .catch(err => {
                             msg.sendMsg("PU_IMPOSSIBLE", message, dataServer);
                             return console.error(err);
-                        }); 
-            } else {
-                return await msg.sendMsg("PUNISH_Y", message, dataServer);
-            }
-        } else {
-            return await msg.sendMsg("PU_NO_USER", message, dataServer);
-        }
-    } else {
-        return await msg.sendMsg("PU_NO_MENTION", message, dataServer);
-    }
+                        });
 }
 
 exports.help = {
