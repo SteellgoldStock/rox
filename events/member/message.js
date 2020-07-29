bd4043050fa6bbb3f72da8e72b6bf10e8ea5c1aa
morcheysha77 <laurentmorgan77@gmail.com>
2020-07-29 15:43:19 +0100

    Test

--- events/member/message.js
+++ events/member/message.js
@@ -27,12 +27,44 @@ client.on("message", message => {
             } else if (results.length > 0) {
                 return message.channel.send("Vous êtes blacklist du bot CHEH") // Message a edit via en.js
             } else {
-                if(!message.member.roles.cache.find(role => role.name === 'MUTE')){
-                    if (!cmd){
-                        return msg.sendXP(message)
+                if (!message.member.roles.cache.find(role => role.name === 'MUTE')) {
+                    if (!cmd) {
+                        let sql = `SELECT * FROM servers WHERE guildid = ${message.guild.id}`;
+                        database.query(sql, (error, results, fields) => {
+                            if (error) {
+                                return console.error(error.message);
+                            }
+
+                            if (results[0].sysXp === 1) {
+                                let sqlselect = `SELECT * FROM servers_xp WHERE guildid = ${message.guild.id} AND userid = ${message.author.id}`;
+                                database.query(sqlselect, (error, resultsXp, fields) => {
+                                    if (error) {
+                                        return console.error(error.message);
+                                    }
+
+                                    let MaxXp = resultsXp[0].level * 150 + resultsXp[0].level * 35
+
+                                    if (resultsXp[0].xp >= MaxXp) {
+                                        const toAddLvl = resultsXp[0].level;
+                                        let sqladdlvl = `UPDATE servers_xp SET level=${toAddLvl} + 1 WHERE userid = ${message.author.id} AND guildid = ${message.guild.id}`
+                                        database.query(sqladdlvl);
+
+                                        msg.sendMsgA(results[0].levelUpMsg.allReplace({
+                                            "{mention}": "<@" + message.author.id + ">",
+                                            "{username}": message.author.name,
+                                            "{guildName}": message.guild.name,
+                                            "{level}": resultsXp[0].level + 1
+                                        }), message)
+                                    }
+
+                                    const toAdd = resultsXp[0].xp;
+                                    let sqladd = `UPDATE servers_xp SET xp=${toAdd} + 1 WHERE userid = ${message.author.id} AND guildid = ${message.guild.id}`
+                                    database.query(sqladd);
+                                });
+                            }
+                        });
                     } else {
-                        switch (cmd.help.type)
-                        {
+                        switch (cmd.help.type) {
                             default:
                                 cmd.run(client, message, args, fs, colors, database, dataServer, language);
                                 break;
@@ -45,3 +77,12 @@ client.on("message", message => {
         });
     });
 });
+
+
+String.prototype.allReplace = function (obj) {
+    var retStr = this;
+    for (var x in obj) {
+        retStr = retStr.replace(new RegExp(x, 'g'), obj[x]);
+    }
+    return retStr;
+};
