d065d78e956bfd2f91badefc7d5647a289cddc65
UnknowG <unknowgdev@gmail.com>
2020-07-30 01:06:18 +0200

    Add Memes command

--- commands/fun/Memes.js
+++ commands/fun/Memes.js
@@ -6,7 +6,6 @@ const got = require('got');
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
     got('https://www.reddit.com/r/memes/random/.json').then(response => {
         let content = JSON.parse(response.body);
-        let memeUrl = `https://reddit.com${permalink}`;
         let memeImage = content[0].data.children[0].data.url;
         let memeTitle = content[0].data.children[0].data.title;
         message.channel.send(":joy: " + memeTitle + "\n" + memeImage);
