13ee6185faa7dd93c5d3c4abeed0e59aafc430c6
UnknowG <unknowgdev@gmail.com>
2020-07-30 13:18:25 +0200

    Truc bidule

--- commands/xp/Level.js
+++ commands/xp/Level.js
@@ -5,18 +5,15 @@ const { Canvas } = require('canvas-constructor');
 const { MessageAttachment } = require('discord.js');
 const fetch = require('node-fetch');
 var path = require('path');
+const star = [];
 
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
     if (!message.guild) return;
 
     Canvas.registerFont('font/Heroes.ttf', {"family": "Heroes"})
-
     let db = JSON.parse(fs.readFileSync("database/users/users.json", "utf8"));
-
     if(message.mentions.users.first()){
-
         let member = message.guild.member(message.mentions.users.first()).user;
-
         if(!db[member.id]){
             db[member.id] = {type:"color",color:`BF5E45`}
             fs.writeFileSync("database/users/users.json", JSON.stringify(db), "utf-8");
@@ -26,7 +23,6 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
         }
 
     } else {
-
         let member = message.author;
 
         if(!db[member.id]){
@@ -47,26 +43,14 @@ async function send(option, message, database, db, member){
         const Attach = new MessageAttachment(buffer, filename);
         await message.channel.send(Attach);
     }else{
-        let sql = `SELECT * FROM goldUsers WHERE userid = ${message.author.id}`;
-        database.query(sql, function (error, results, fields) {
-            if (error) {
-                return console.log(error);
-            } else if (results.length > 0) {
-                exports.isGold = "true";
-            }else{
-                exports.isGold = "false"
-            }
-        });
+        const buffer = await color(message, database, db, member, star[message.author.id].asStar);
+        const filename = `${message.id}.png`;
+        const Attach = new MessageAttachment(buffer, filename);
+        await message.channel.send(Attach);
     }
-
-    console.log(exports.isGold);
-    const buffer = await color(message, database, db, member, exports.isGold);
-    const filename = `${message.id}.png`;
-    const Attach = new MessageAttachment(buffer, filename);
-    await message.channel.send(Attach);
 }
 
-async function color(message, database, db, member, isGold) {
+async function color(message, database, db, member) {
 
     database.query(`SELECT * FROM servers_xp WHERE guildid=${message.guild.id} AND userid=${member.id}`, function (error, results, fields) {
         if (error) {
@@ -86,59 +70,30 @@ async function color(message, database, db, member, isGold) {
 
     const name = member.username.length > 9 ? member.username.substring(0, 17) + '...'
         : member.username;
-
-    if(isGold == "true"){
-        return new Canvas(400, 180)
-            .setColor("#" + db[member.id].color)
-            .addRect(84, 0, 316, 180)
-            .setColor("#36393F")
-            .addRect(169, 26, 231, 46)
-            .addRect(224, 108, 176, 46)
-            .setShadowColor('rgba(22, 22, 22, 1)')
-            .setShadowOffsetY(5)
-            .setShadowBlur(10)
-            .addCircle(84, 90, 62)
-            .addCircularImage(avatar, 85, 90, 64)
-            .addImage(path.join(`database/users/gold.png`), 12,40,36,36)
-            .save()
-            .createBeveledClip(10, 139, 150, 30, 0)
-            .setColor('#36393F')
-            .fill()
-            .restore()
-            .setTextAlign('center')
-            .setTextFont('12pt Heroes')
-            .setColor('#FFFFFF')
-            .addText(name, 285, 54)
-            .addText(`Level: ${exports.level}`, 84, 157)
-            .setTextAlign('left')
-            .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
-            .toBuffer();
-    }else{
-        return new Canvas(400, 180)
-            .setColor("#" + db[member.id].color)
-            .addRect(84, 0, 316, 180)
-            .setColor("#36393F")
-            .addRect(169, 26, 231, 46)
-            .addRect(224, 108, 176, 46)
-            .setShadowColor('rgba(22, 22, 22, 1)')
-            .setShadowOffsetY(5)
-            .setShadowBlur(10)
-            .addCircle(84, 90, 62)
-            .addCircularImage(avatar, 85, 90, 64)
-            .save()
-            .createBeveledClip(10, 139, 150, 30, 0)
-            .setColor('#36393F')
-            .fill()
-            .restore()
-            .setTextAlign('center')
-            .setTextFont('12pt Heroes')
-            .setColor('#FFFFFF')
-            .addText(name, 285, 54)
-            .addText(`Level: ${exports.level}`, 84, 157)
-            .setTextAlign('left')
-            .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
-            .toBuffer();
-    }
+    return new Canvas(400, 180)
+        .setColor("#" + db[member.id].color)
+        .addRect(84, 0, 316, 180)
+        .setColor("#36393F")
+        .addRect(169, 26, 231, 46)
+        .addRect(224, 108, 176, 46)
+        .setShadowColor('rgba(22, 22, 22, 1)')
+        .setShadowOffsetY(5)
+        .setShadowBlur(10)
+        .addCircle(84, 90, 62)
+        .addCircularImage(avatar, 85, 90, 64)
+        .save()
+        .createBeveledClip(10, 139, 150, 30, 0)
+        .setColor('#36393F')
+        .fill()
+        .restore()
+        .setTextAlign('center')
+        .setTextFont('12pt Heroes')
+        .setColor('#FFFFFF')
+        .addText(name, 285, 54)
+        .addText(`Level: ${exports.level}`, 84, 157)
+        .setTextAlign('left')
+        .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+        .toBuffer();
 }
 async function image(message, database, member) {
 
