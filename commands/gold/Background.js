2d6f6d39566c7b3e4448b44c2a767c169f6afef5
morcheysha77 <laurentmorgan77@gmail.com>
2020-07-30 17:52:36 +0100

    Background

--- commands/gold/Background.js
+++ commands/gold/Background.js
@@ -11,7 +11,7 @@ let request = require(`request`);
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
     let type = args[0];
     if(!type){ return await msg.sendMsg("MISSED_ARGUMENTS",message,dataServer)}
-    const types = ["color","img"];
+    const types = ["color","img", "server"];
     if(!types.includes(type)){ return await msg.sendMsg("ARGS_BACKGROUND",message,dataServer) }
 
     switch (type){
@@ -48,6 +48,21 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
                     return msg.sendMsg("NOT_GOLD_USER",message,dataServer)
                 }
             });
+            break
+
+        case "server":
+            if(dataServer.isGold === 1) {
+                    if(!message.attachments.first()){ return msg.sendMsg("NOT_IMG",message,dataServer) }
+
+                    if(path.extname(message.attachments.first().url) == ".png" || path.extname(message.attachments.first().url) == ".jpg" || path.extname(message.attachments.first().url) == ".jpeg"){
+                        msg.sendMsg("CHECK_PICTURE",message,dataServer)
+                        fnS(message.attachments.first().url,msg,language,message.attachments.first(),message, dataServer,message.id, message.channel.id)
+                    }else{
+                        return msg.sendMsg("NOT_IMG",message,dataServer)
+                    }
+                } else {
+                    return msg.sendMsg("NOT_GOLD_SERVER",message,dataServer)
+                }
             break
     }
 }
@@ -98,6 +113,42 @@ async function fn(url, msg, language, attachement, message, dataServer, msgId, c
 
      await download(message.attachments.first().url, message.author.id);
      return msg.sendMsgA(language("DOWNLANDED",dataServer.prefix),message,dataServer);
+}
+
+async function fnS(url, msg, language, attachement, message, dataServer, msgId, chaId) {
+    const pic = await axios.get(url, {
+        responseType: 'arraybuffer',
+    })
+
+    const model = await nsfw.load()
+    const image = await tf.node.decodeImage(pic.data,3)
+    const predictions = await model.classify(image)
+    image.dispose()
+
+    if(predictions[0].className == "Hentai" || predictions[0].className == "Porn"){
+        if(predictions[0].probability >= 0.5000000000000000){
+            client.channels.cache.get(chaId).messages.fetch(msgId).then(message => message.delete())
+
+            return await msg.sendMsg("PICTURE_NOT_ALLOWED",message,dataServer)
+        }
+    }
+
+    let db = JSON.parse(fs.readFileSync("database/users/users.json", "utf8"));
+
+    message.guild.users.cache.forEach(member => {
+
+        if(!db[member.user.id]){
+            db[member.user.id] = {type:"img",color:null}
+        }else{
+            db[member.user.id].type = "img";
+        }
+
+        fs.writeFileSync("database/users/users.json", JSON.stringify(db), "utf-8");
+
+    });
+
+    await download(message.attachments.first().url, message.guild.id);
+    return msg.sendMsgA(language("DOWNLANDED",dataServer.prefix),message,dataServer);
 }
 
 exports.help = {
