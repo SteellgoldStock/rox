441d58effad812a3f2c15a8e8c3261e6a537a342
UnknowG <unknowgdev@gmail.com>
2020-07-30 17:56:44 +0200

    Edit Perm

--- commands/admin/Messages.js
+++ commands/admin/Messages.js
@@ -2,31 +2,40 @@ const Discord = require("discord.js");
 const { client, database, msg, colors, fs} = require("../../rox");
 
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
-    if(!message.member.roles.cache.has(dataServer.adminRole) || !message.member.hasPermission('ADMINISTRATOR')){
+    if (message.member.roles.cache.has(dataServer.adminRole) || message.member.hasPermission('ADMINISTRATOR')) {
+
+        if (!args[0]) {
+            return await msg.sendMsg("INVALID_ARGS_TEXTS", message, dataServer)
+        }
+
+        switch (args[0]) {
+            case "joinText":
+                if (!args[1]) {
+                    return await msg.sendMsgA(language("MISSED_TEXT", dataServer.prefix, "jq"), message, dataServer)
+                }
+                await update("join", args.slice(1).join(" "), message.guild.id);
+                await msg.sendMsg("UPDATED", message, dataServer);
+                break;
+            case "quitText":
+                if (!args[1]) {
+                    return await msg.sendMsgA(language("MISSED_TEXT", dataServer.prefix, "jq"), message, dataServer)
+                }
+                await update("quit", args.slice(1).join(" "), message.guild.id);
+                await msg.sendMsg("UPDATED", message, dataServer);
+                break;
+            case "lvlUp":
+                if (!args[1]) {
+                    return await msg.sendMsgA(language("MISSED_TEXT", dataServer.prefix, "lvlup"), message, dataServer)
+                }
+                await update("lvlUp", args.slice(1).join(" "), message.guild.id);
+                await msg.sendMsg("UPDATED", message, dataServer);
+                break;
+            default:
+                await msg.sendMsg("INVALID_ARGS_TEXTS", message, dataServer)
+                break;
+        }
+    } else {
         return await msg.sendMsg("PERMISSION_DENIED", message, dataServer);
-    }
-
-    if(!args[0]){ return await msg.sendMsg("INVALID_ARGS_TEXTS",message,dataServer)}
-
-    switch (args[0]) {
-        case "joinText":
-            if(!args[1]){ return await msg.sendMsgA(language("MISSED_TEXT",dataServer.prefix,"jq"),message,dataServer) }
-            await update("join",args.slice(1).join(" "),message.guild.id);
-            await msg.sendMsg("UPDATED",message,dataServer);
-            break;
-        case "quitText":
-            if(!args[1]){ return await msg.sendMsgA(language("MISSED_TEXT",dataServer.prefix,"jq"),message,dataServer) }
-            await update("quit",args.slice(1).join(" "),message.guild.id);
-            await msg.sendMsg("UPDATED",message,dataServer);
-            break;
-        case "lvlUp":
-            if(!args[1]){ return await msg.sendMsgA(language("MISSED_TEXT",dataServer.prefix,"lvlup"),message,dataServer) }
-            await update("lvlUp",args.slice(1).join(" "),message.guild.id);
-            await msg.sendMsg("UPDATED",message,dataServer);
-            break;
-        default:
-            await msg.sendMsg("INVALID_ARGS_TEXTS",message,dataServer)
-            break;
     }
 }
 
--- commands/admin/Roles.js
+++ commands/admin/Roles.js
@@ -1,32 +1,40 @@
 const Discord = require("discord.js");
 const { client, database, msg, colors, fs} = require("../../rox");
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
-    if(!message.member.roles.cache.has(dataServer.adminRole) || !message.member.hasPermission('ADMINISTRATOR')){
+    if (message.member.roles.cache.has(dataServer.adminRole) || message.member.hasPermission('ADMINISTRATOR')) {
+        if (!args[0]) {
+            return await msg.sendMsg("INVALID_ARGS_ROLES", message, dataServer)
+        }
+        const mentionedRole = message.mentions.roles.first();
+
+        switch (args[0]) {
+            case "adminRole":
+                if (!mentionedRole) {
+                    return await msg.sendMsg("MENTION_ROLE", message, dataServer)
+                }
+                await update("ar", mentionedRole.id, message.guild.id);
+                await msg.sendMsg("UPDATED", message, dataServer);
+                break;
+            case "modRole":
+                if (!mentionedRole) {
+                    return await msg.sendMsg("MENTION_ROLE", message, dataServer)
+                }
+                await update("mr", mentionedRole.id, message.guild.id);
+                await msg.sendMsg("UPDATED", message, dataServer);
+                break;
+            case "autoRole":
+                if (!mentionedRole) {
+                    return await msg.sendMsg("MENTION_ROLE", message, dataServer)
+                }
+                await update("aur", mentionedRole.id, message.guild.id);
+                await msg.sendMsg("UPDATED", message, dataServer);
+                break;
+            default:
+                await msg.sendMsg("INVALID_ARGS_ROLES", message, dataServer)
+                break;
+        }
+    } else {
         return await msg.sendMsg("PERMISSION_DENIED", message, dataServer);
-    }
-
-    if(!args[0]){ return await msg.sendMsg("INVALID_ARGS_ROLES",message,dataServer)}
-    const mentionedRole = message.mentions.roles.first();
-
-    switch (args[0]) {
-        case "adminRole":
-            if(!mentionedRole){ return await msg.sendMsg("MENTION_ROLE",message,dataServer) }
-            await update("ar",mentionedRole.id,message.guild.id);
-            await msg.sendMsg("UPDATED",message,dataServer);
-            break;
-        case "modRole":
-            if(!mentionedRole){ return await msg.sendMsg("MENTION_ROLE",message,dataServer) }
-            await update("mr",mentionedRole.id,message.guild.id);
-            await msg.sendMsg("UPDATED",message,dataServer);
-            break;
-        case "autoRole":
-            if(!mentionedRole){ return await msg.sendMsg("MENTION_ROLE",message,dataServer) }
-            await update("aur",mentionedRole.id,message.guild.id);
-            await msg.sendMsg("UPDATED",message,dataServer);
-            break;
-        default:
-            await msg.sendMsg("INVALID_ARGS_ROLES",message,dataServer)
-            break;
     }
 }
 
