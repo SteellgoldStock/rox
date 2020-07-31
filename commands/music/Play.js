9de7e0950216bbcc1da7249680b0618897d9abf1
UnknowG <unknowgdev@gmail.com>
2020-07-31 13:45:05 +0200

    Add FFMPEG & YTBCORE

---
+++ commands/music/Play.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const { client, database, msg, colors, fs, team} = require("../../rox");
+const { MessageAttachment } = require('discord.js');
+const got = require('got');
+
+module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
+
+}
+
+
+exports.help = {
+    name: "play",
+}
