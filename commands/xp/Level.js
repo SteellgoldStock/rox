a40538a104afacd86be6f5fa4793e07847230e0d
UnknowG <unknowgdev@gmail.com>
2020-07-31 13:42:14 +0200

    Level clc

--- commands/xp/Level.js
+++ commands/xp/Level.js
@@ -37,7 +37,7 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
 async function send(option, message, database, db, member, dataServer){
 
     if(option == "img"){
-        const buffer = await image(message, database, member, db);
+        const buffer = await image(message, database, member);
         const filename = `${member.id}.png`;
         const Attach = new MessageAttachment(buffer, filename);
         await message.channel.send(Attach);
@@ -83,26 +83,117 @@ async function color(message, database, db, member, dataServer) {
 
         if (exports.gold === member.id){
 
-            await addImageGold(message.guild.id, member)
+            return new Canvas(400, 180)
+                .addImage(path.join(`database/users/backgrounds/${message.guild.id}.png`), 84, 0, 316, 180)
+                .setColor("#36393F")
+                .addRect(169, 26, 231, 46)
+                .addRect(224, 108, 176, 46)
+                .setShadowColor('rgba(22, 22, 22, 1)')
+                .setShadowOffsetY(5)
+                .setShadowBlur(10)
+                .addCircle(84, 90, 62)
+                .addCircularImage(avatar, 85, 90, 64)
+                .addImage(path.join(`database/users/gold.png`), 12,40,36,36)
+                .save()
+                .createBeveledClip(10, 139, 150, 30, 0)
+                .setColor('#36393F')
+                .fill()
+                .restore()
+                .setTextAlign('center')
+                .setTextFont('12pt Heroes')
+                .setColor('#FFFFFF')
+                .addText(name, 285, 54)
+                .addText(`Level: ${exports.level}`, 84, 157)
+                .setTextAlign('left')
+                .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+                .toBuffer();
 
         } else {
 
-            await addImage(message.guild.id, member)
-
+            return new Canvas(400, 180)
+                .addImage(path.join(`database/users/backgrounds/${message.guild.id}.png`), 84, 0, 316, 180)
+                .setColor("#36393F")
+                .addRect(169, 26, 231, 46)
+                .addRect(224, 108, 176, 46)
+                .setShadowColor('rgba(22, 22, 22, 1)')
+                .setShadowOffsetY(5)
+                .setShadowBlur(10)
+                .addCircle(84, 90, 62)
+                .addCircularImage(avatar, 85, 90, 64)
+                .save()
+                .createBeveledClip(10, 139, 150, 30, 0)
+                .setColor('#36393F')
+                .fill()
+                .restore()
+                .setTextAlign('center')
+                .setTextFont('12pt Heroes')
+                .setColor('#FFFFFF')
+                .addText(name, 285, 54)
+                .addText(`Level: ${exports.level}`, 84, 157)
+                .setTextAlign('left')
+                .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+                .toBuffer();
         }
 
     } else if (exports.gold === member.id){
 
-        await addColorGold(member.id, db, member)
+        return new Canvas(400, 180)
+            .setColor("#" + db[member.id].color)
+            .addRect(84, 0, 316, 180)
+            .setColor("#36393F")
+            .addRect(169, 26, 231, 46)
+            .addRect(224, 108, 176, 46)
+            .setShadowColor('rgba(22, 22, 22, 1)')
+            .setShadowOffsetY(5)
+            .setShadowBlur(10)
+            .addCircle(84, 90, 62)
+            .addCircularImage(avatar, 85, 90, 64)
+            .addImage(path.join(`database/users/gold.png`), 12,40,36,36)
+            .save()
+            .createBeveledClip(10, 139, 150, 30, 0)
+            .setColor('#36393F')
+            .fill()
+            .restore()
+            .setTextAlign('center')
+            .setTextFont('12pt Heroes')
+            .setColor('#FFFFFF')
+            .addText(name, 285, 54)
+            .addText(`Level: ${exports.level}`, 84, 157)
+            .setTextAlign('left')
+            .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+            .toBuffer();
 
     } else {
 
-        await addColor(member.id, db, member)
+        return new Canvas(400, 180)
+            .setColor("#" + db[member.id].color)
+            .addRect(84, 0, 316, 180)
+            .setColor("#36393F")
+            .addRect(169, 26, 231, 46)
+            .addRect(224, 108, 176, 46)
+            .setShadowColor('rgba(22, 22, 22, 1)')
+            .setShadowOffsetY(5)
+            .setShadowBlur(10)
+            .addCircle(84, 90, 62)
+            .addCircularImage(avatar, 85, 90, 64)
+            .save()
+            .createBeveledClip(10, 139, 150, 30, 0)
+            .setColor('#36393F')
+            .fill()
+            .restore()
+            .setTextAlign('center')
+            .setTextFont('12pt Heroes')
+            .setColor('#FFFFFF')
+            .addText(name, 285, 54)
+            .addText(`Level: ${exports.level}`, 84, 157)
+            .setTextAlign('left')
+            .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+            .toBuffer();
 
     }
 }
 
-async function image(message, database, member, db) {
+async function image(message, database, member) {
 
     database.query(`SELECT * FROM goldUsers WHERE userid = ${member.id}`, function (error, results, fields) {
         if (error) {
@@ -135,163 +226,67 @@ async function image(message, database, member, db) {
 
     if (exports.gold === member.id){
 
-        await addImageGold(member.id, member);
+        return new Canvas(400, 180)
+            .addImage(path.join(`database/users/backgrounds/${member.id}.png`), 84, 0, 316, 180)
+            .setColor("#36393F")
+            .addRect(169, 26, 231, 46)
+            .addRect(224, 108, 176, 46)
+            .setShadowColor('rgba(22, 22, 22, 1)')
+            .setShadowOffsetY(5)
+            .setShadowBlur(10)
+            .addCircle(84, 90, 62)
+            .addCircularImage(avatar, 85, 90, 64)
+            .addImage(path.join(`database/users/gold.png`), 12,40,36,36)
+            .save()
+            .createBeveledClip(10, 139, 150, 30, 0)
+            .setColor('#36393F')
+            .fill()
+            .restore()
+            .setTextAlign('center')
+            .setTextFont('12pt Heroes')
+            .setColor('#FFFFFF')
+            .addText(name, 285, 54)
+            .addText(`Level: ${exports.level}`, 84, 157)
+            .setTextAlign('left')
+            .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+            .toBuffer();
 
     } else {
+
+        return new Canvas(400, 180)
+            .setColor("#BF5E45")
+            .addRect(84, 0, 316, 180)
+            .setColor("#36393F")
+            .addRect(169, 26, 231, 46)
+            .addRect(224, 108, 176, 46)
+            .setShadowColor('rgba(22, 22, 22, 1)')
+            .setShadowOffsetY(5)
+            .setShadowBlur(10)
+            .addCircle(84, 90, 62)
+            .addCircularImage(avatar, 85, 90, 64)
+            .save()
+            .createBeveledClip(10, 139, 150, 30, 0)
+            .setColor('#36393F')
+            .fill()
+            .restore()
+            .setTextAlign('center')
+            .setTextFont('12pt Heroes')
+            .setColor('#FFFFFF')
+            .addText(name, 285, 54)
+            .addText(`Level: ${exports.level}`, 84, 157)
+            .setTextAlign('left')
+            .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
+            .toBuffer();
 
         let dbi = JSON.parse(fs.readFileSync("database/users/users.json", "utf8"));
         dbi[member.id] = {type:"color",color:`BF5E45`};
         fs.writeFileSync("database/users/users.json", JSON.stringify(dbi), "utf-8");
 
-        await addColor(member.id, db, member);
-
-
     }
 }
 
 function kFormatter(num) {
-
     return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
-}
-
-async function addColor(id, db, member) {
-
-    const result = await fetch(member.displayAvatarURL({format: 'png'}));
-    if (!result.ok) new Error('Failed to get the avatar!');
-    const avatar = await result.buffer();
-
-    const name = member.username.length > 9 ? member.username.substring(0, 17) + '...'
-        : member.username;
-
-    return new Canvas(400, 180)
-        .setColor("#" + db[id].color)
-        .addRect(84, 0, 316, 180)
-        .setColor("#36393F")
-        .addRect(169, 26, 231, 46)
-        .addRect(224, 108, 176, 46)
-        .setShadowColor('rgba(22, 22, 22, 1)')
-        .setShadowOffsetY(5)
-        .setShadowBlur(10)
-        .addCircle(84, 90, 62)
-        .addCircularImage(avatar, 85, 90, 64)
-        .save()
-        .createBeveledClip(10, 139, 150, 30, 0)
-        .setColor('#36393F')
-        .fill()
-        .restore()
-        .setTextAlign('center')
-        .setTextFont('12pt Heroes')
-        .setColor('#FFFFFF')
-        .addText(name, 285, 54)
-        .addText(`Level: ${exports.level}`, 84, 157)
-        .setTextAlign('left')
-        .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
-        .toBuffer();
-}
-
-async function addColorGold(id, db, member) {
-
-    const result = await fetch(member.displayAvatarURL({format: 'png'}));
-    if (!result.ok) new Error('Failed to get the avatar!');
-    const avatar = await result.buffer();
-
-    const name = member.username.length > 9 ? member.username.substring(0, 17) + '...'
-        : member.username;
-
-    return new Canvas(400, 180)
-        .setColor("#" + db[id].color)
-        .addRect(84, 0, 316, 180)
-        .setColor("#36393F")
-        .addRect(169, 26, 231, 46)
-        .addRect(224, 108, 176, 46)
-        .setShadowColor('rgba(22, 22, 22, 1)')
-        .setShadowOffsetY(5)
-        .setShadowBlur(10)
-        .addCircle(84, 90, 62)
-        .addCircularImage(avatar, 85, 90, 64)
-        .addImage(path.join(`database/users/gold.png`), 12,40,36,36)
-        .save()
-        .createBeveledClip(10, 139, 150, 30, 0)
-        .setColor('#36393F')
-        .fill()
-        .restore()
-        .setTextAlign('center')
-        .setTextFont('12pt Heroes')
-        .setColor('#FFFFFF')
-        .addText(name, 285, 54)
-        .addText(`Level: ${exports.level}`, 84, 157)
-        .setTextAlign('left')
-        .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
-        .toBuffer();
-}
-
-async function addImage(id, member) {
-
-    const result = await fetch(member.displayAvatarURL({format: 'png'}));
-    if (!result.ok) new Error('Failed to get the avatar!');
-    const avatar = await result.buffer();
-
-    const name = member.username.length > 9 ? member.username.substring(0, 17) + '...'
-        : member.username;
-
-    return new Canvas(400, 180)
-        .addImage(path.join(`database/users/backgrounds/${id}.png`), 84, 0, 316, 180)
-        .setColor("#36393F")
-        .addRect(169, 26, 231, 46)
-        .addRect(224, 108, 176, 46)
-        .setShadowColor('rgba(22, 22, 22, 1)')
-        .setShadowOffsetY(5)
-        .setShadowBlur(10)
-        .addCircle(84, 90, 62)
-        .addCircularImage(avatar, 85, 90, 64)
-        .save()
-        .createBeveledClip(10, 139, 150, 30, 0)
-        .setColor('#36393F')
-        .fill()
-        .restore()
-        .setTextAlign('center')
-        .setTextFont('12pt Heroes')
-        .setColor('#FFFFFF')
-        .addText(name, 285, 54)
-        .addText(`Level: ${exports.level}`, 84, 157)
-        .setTextAlign('left')
-        .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
-        .toBuffer();
-}
-
-async function addImageGold(id, member) {
-
-    const result = await fetch(member.displayAvatarURL({format: 'png'}));
-    if (!result.ok) new Error('Failed to get the avatar!');
-    const avatar = await result.buffer();
-
-    const name = member.username.length > 9 ? member.username.substring(0, 17) + '...'
-        : member.username;
-
-    return new Canvas(400, 180)
-        .addImage(path.join(`database/users/backgrounds/${id}.png`), 84, 0, 316, 180)
-        .setColor("#36393F")
-        .addRect(169, 26, 231, 46)
-        .addRect(224, 108, 176, 46)
-        .setShadowColor('rgba(22, 22, 22, 1)')
-        .setShadowOffsetY(5)
-        .setShadowBlur(10)
-        .addCircle(84, 90, 62)
-        .addCircularImage(avatar, 85, 90, 64)
-        .addImage(path.join(`database/users/gold.png`), 12,40,36,36)
-        .save()
-        .createBeveledClip(10, 139, 150, 30, 0)
-        .setColor('#36393F')
-        .fill()
-        .restore()
-        .setTextAlign('center')
-        .setTextFont('12pt Heroes')
-        .setColor('#FFFFFF')
-        .addText(name, 285, 54)
-        .addText(`Level: ${exports.level}`, 84, 157)
-        .setTextAlign('left')
-        .addText(`XP: ${kFormatter(exports.xp)}`, 241, 136)
-        .toBuffer();
 }
 
 exports.help = {
