fb2a9f6764017f24223106dd080080fd186bbb49
UnknowG <unknowgdev@gmail.com>
2020-08-02 00:52:32 +0200

    Fix #2

--- commands/fun/Door.js
+++ commands/fun/Door.js
@@ -2,15 +2,14 @@ const Discord = require("discord.js");
 const { client, database, msg, colors, fs, team} = require("../../rox");
 const { MessageAttachment } = require('discord.js');
 
-
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
     let member = message.mentions.users.first();
 
     if(!member){
-        msg.sendMsg("PU_NO_MENTION",message,dataServer);
+        message.channel.send(language("DOOR_OPEN") +  " **"+message.author.username+"**" + "\n\nhttp://exit.rox.wtf");
     }
 
-    if(team.includes(member.user.id)){
+    if(team.includes(member.id)){
         return await msg.sendMsg("DOOR_TEAM_NOT",message,dataServer);
     }
 
