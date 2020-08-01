fe9dbaa27126a25ea556c4722312682ac8d70e7e
UnknowG <unknowgdev@gmail.com>
2020-08-01 14:08:36 +0200

    @jordanuniverse refait le !level (héhé)

--- commands/xp/Level.js
+++ commands/xp/Level.js
@@ -9,10 +9,6 @@ var path = require('path');
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
     if (!message.guild) return;
     if(!team.includes(message.author.id)) return message.channel.send("This command is in re-devolvement thank you kindly wait");
-
-    const canvas = createCanvas(1000,333);
-    const ctx = canvas.getContext("2d");
-    const background = await loadImage()
 }
 
 function kFormatter(num) {
