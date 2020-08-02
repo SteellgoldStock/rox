fda83f5f83ab463ab66c040480af07918fc01515
morcheysha77 <laurentmorgan77@gmail.com>
2020-08-02 13:53:42 +0100

    MessageCustomCommand

--- events/member/messageCustomCommands.js
+++ events/member/messageCustomCommands.js
@@ -1,5 +1,7 @@
 const { client, colors, botConfg, fs, database, msg} = require("../../rox");
 
+const figlet = require('figlet');
+
 client.on('message',message => {
     if (!message.guild) return;
     let dbC = JSON.parse(fs.readFileSync("database/ccommands/" + message.guild.id + ".json", "utf8"));
@@ -26,12 +28,39 @@ client.on('message',message => {
                     };
 
                     const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+                    message.guild.members.fetch().then(fetchedMembers => {
+                        const vert = fetchedMembers.filter(member => member.presence.status === 'online').size;
+                        const jaune = fetchedMembers.filter(member => member.presence.status === 'Inactive').size;
+                        const rouge = fetchedMembers.filter(member => member.presence.status === 'dnd').size;
+                        const blanc = fetchedMembers.filter(member => member.presence.status === 'invisible').size;
+                        const totalOnline = vert + jaune + rouge + blanc
+                        const totalOffline = fetchedMembers.filter(member => member.presence.status === 'offline').size;
+                        exports.online = totalOnline;
+                        exports.offline = totalOffline;
+                    });
+
+                    figlet(args.slice(0).join(" "), function(err, data) {
+                        if (err) {
+                            console.log('Something went wrong...');
+                            console.dir(err);
+                            return;
+                        }
+
+                        exports.ascii = data;
+                    });
+
                     message.channel.send(dbC[prop].allReplace(
                         {
                             '{mention}': "<@" + message.author.id + ">",
-                            '{serverName}': message.guild.name,
+                            '{guildName}': message.guild.name,
                             '{username}': message.author.username,
-                            '{sayMessage}': args.slice(1).join(" ")
+                            '{sayMessage}': args.slice(1).join(" "),
+                            '{asciiMessage}': exports.ascii,
+                            '{userCount}': message.guild.memberCount,
+                            '{countOnline}': exports.online,
+                            '{countOffline}': exports.offline
+
                         }))
                 } else {
 
