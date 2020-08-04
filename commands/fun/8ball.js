3b0656772bd3fc4b24bff6dad6d4a8f212fa5ec4
morcheysha77 <laurentmorgan77@gmail.com>
2020-08-04 19:53:46 +0100

    Fix 8ball.js

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -3,7 +3,6 @@ const { client, database, msg, colors, fs, team} = require("../../rox");
 
 module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
     if(!args[0]) return await msg.sendMsg("MISSED_ARGUMENTS",message,dataServer)
-
     let random = Math.floor((Math.random() * 10) + 1)
     const answerNO = parseInt(random, 10);
     return await msg.sendMsg(`RESPONSE_${answerNO}`, message, dataServer);
