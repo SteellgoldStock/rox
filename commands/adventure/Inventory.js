f5d93b951dcb7b61eeb9f607b17361c5a42fbd89
UnknowG <unknowgdev@gmail.com>
2020-08-04 20:40:49 +0200

    Object

--- commands/adventure/Inventory.js
+++ commands/adventure/Inventory.js
@@ -10,9 +10,14 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
         if (error) {
             return console.error(error.message);
         }else if(results.length > 0){
+            const job = jobFormater(results[0].job,language);
+            console.log(job);
+            const house = houseFormater(results[0].house,language);
+            console.log(house);
+
             let embed = new Discord.MessageEmbed()
             .setTitle("Inventory of " + message.author.username)
-            .addField("Stats:", `<:gems:740261046480142377> Gems: ${results[0].gems}\n:wrench: ${jobFormater(results[0].job, language)}\n:house: ${houseFormater(results[0].house, language)}\\n`)
+            .addField("Stats:", `<:gems:740261046480142377> Gems: ${results[0].gems}\n:wrench: ${job}\n:house: ${house}\\n`)
 
             message.channel.send(embed)
         }else{
