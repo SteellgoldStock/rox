e50a1346440c4147930c3a5285ca28f0a7e875f3
UnknowG <unknowgdev@gmail.com>
2020-08-12 18:25:48 +0200

    Delete Channel Event

--- events/guild/channelDelete.js
+++ events/guild/channelDelete.js
@@ -2,15 +2,29 @@ const Discord = require('discord.js');
 const {colors, client, fs, database, msg} = require('./../../rox')
 
 client.on('channelDelete', channel => {
-    /** SERVER **/
+    /** INTER SERVER **/
     database.query(`SELECT * FROM servers WHERE interServerChannel = ${channel.id}`, function (error, results, fields) {
         if (error) {
             return false;
         } else if (results.length > 0) {
-            console.log(channel.name)
-
             if(results[0].interServerChannel == channel.id){
                 var eventCHANNELDELETE = `UPDATE servers SET interServerChannel = 'false' WHERE interServerChannel = '${channel.id}'`;
+                return database.query(eventCHANNELDELETE, function (err) {
+                    if (err) throw err;
+                });
+            }
+        } else {
+
+        }
+    });
+
+    database.query(`SELECT * FROM servers WHERE announceChannel = ${channel.id}`, function (error, results, fields) {
+        if (error) {
+            return false;
+        } else if (results.length > 0) {
+
+            if(results[0].announceChannel == channel.id){
+                var eventCHANNELDELETE = `UPDATE servers SET announceChannel = 'false' WHERE announceChannel = '${channel.id}'`;
                 return database.query(eventCHANNELDELETE, function (err) {
                     if (err) throw err;
                 });
