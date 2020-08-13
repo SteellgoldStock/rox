34610b522ff947a454349002ec95b54355dd5eca
UnknowG <unknowgdev@gmail.com>
2020-08-14 00:10:33 +0200

    Edit welcome embed bot

---
+++ commands/basic/FR_Start.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const { client, database, msg, colors, fs} = require("../../rox");
+
+module.exports.run = async (client, message, args, fs, colors, database, dataServer, language) => {
+    if(!message.guild) return;
+
+    let embed = new Discord.MessageEmbed()
+        .setTitle("Salut !")
+        .setDescription("Merci de m'avoir ajouté, j'espère que je ne vous décevrai pas.")
+        .addField("<:rox:737051270980042783> Invite","Pour m'inviter, c'est très simple, utilisez la commande `!invite` pour recevoir [le lien d'invitation ou cliquer simplement ici](https://discord.com/api/oauth2/authorize?client_id=733760070503890994&permissions=8&scope=bot), une question **pourquoi je demande la permission de l'administrateur?** car j'ai énormement de fonctionnalitées, donc pour éviter de deviner quel permission il faut pour tel fonctionnalitées, le meuilleur est de mettre administrateur, pendant ma phase de bêta tout les serveurs ou j'était, on m'a donné la permission administrateur, et il n'y a eu aucun problème, cependant si vous ne me faite toujours pas confiance je vous donne ce lien au quel vous pourrez choisir les permissions [cliquer ici](https://discord.com/api/oauth2/authorize?client_id=733760070503890994&permissions=2147483639&scope=bot) en qui vous n'avez pas confiance ou qui ne me fait pas confiance, qui sera votre libre choix")
+        .addField("<:book3:733698761171271750> Support","Avez-vous besoin d'aide ? Si oui, je vous invite à vous joindre à mon [discord](https://discord.gg/NVBwmFz) qui est fait pour cela, ou aller voir mon [wiki](https://doc.rox.wtf), il est très bien fourni!")
+        .addField("<a:config:738108401208262738> Configuration","Pour commencer à utiliser le bot, configurez un rôle d'administrateur et un autre de modérateur, avec la commande `!roles` qui n'est utilisable que par les *aminRole*, et les personnes ayant la permission `ADMINISTRATOR`")
+        .setTimestamp()
+        .setColor("#4e64b5")
+        .setFooter('Rox • ' + msg.version,client.user.avatarURL())
+    message.channel.send(embed)
+    message.channel.send(":flag_us: For a english version of the message, execute the command `!us_start`")
+
+}
+
+exports.help = {
+    name: "fr_start",
+}
--- events/client/guildCreate.js
+++ events/client/guildCreate.js
@@ -14,8 +14,12 @@ client.on('guildCreate',(guild) => {
         } else {
             sendWelcMsg(guild).then(r => console.log("Welcome message is send"));
 
+            const GUILD_NAME = Buffer.from(guild.name).toString('base64');
+            console.log(GUILD_NAME)
 
             var postServer = {
+                tag: "IA==",
+                name: GUILD_NAME,
                 guildid: guild.id,
                 lang: "en",
                 prefix: "!",
@@ -27,6 +31,7 @@ client.on('guildCreate',(guild) => {
                 quitText: "Tm8uLiB7dXNlcm5hbWV9IGhhcyBsZWF2ZWQge2d1aWxkTmFtZX0=",
                 interServerChannel: "false",
                 announceChannel: "false",
+                logsChannel: "false",
                 ticketCat: "----- Ticket -----",
                 commandsChannel: "false",
 
@@ -53,7 +58,6 @@ client.on('guildCreate',(guild) => {
                 level: 1,
                 xp: 0
             };
-
             database.query('INSERT INTO servers SET ?', postServer, function (error, results, fields) {
                 if (error) throw error;
             });
@@ -61,8 +65,10 @@ client.on('guildCreate',(guild) => {
     });
 
     let dbC = {};
+    let dbR = {};
 
     fs.writeFileSync("database/ccommands/" + guild.id + ".json", JSON.stringify(dbC), "utf-8");
+    fs.writeFileSync("database/rlevels/" + guild.id + ".json", JSON.stringify(dbR), "utf-8");
 
 
     /** USERS XP BY SERVER **/
@@ -94,17 +100,17 @@ client.on('guildCreate',(guild) => {
 });
 
 async function sendWelcMsg(guild){
-    const channel = guild.channels.cache.filter(c => c.type === 'text').find(x => x.position == 0);
-    const channel1 = guild.channels.cache.filter(c => c.type === 'text').find(x => x.position == 1);
+    const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
 
     let embed = new Discord.MessageEmbed()
         .setTitle("Hello !")
-        .setDescription("Thanks for adding me, so you have been chosen as a tester! All the bot features are not yet available!")
-        .addField("<:rox:737051270980042783> Invite","You can't yet add me on other servers than those that the bot team has chosen, you can however come and test with us, on the support server")
-        .addField("<:berry:737050710528753766> Support","For help join our discord by clicking [here](https://discord.gg/Yee8dWA), or use `!help`")
+        .setDescription("Thanks for adding me, i hope I don't disappoint you.")
+        .addField("<:rox:737051270980042783> Invite","To invite me it's very simple, use the command `!invite` [to receive the invitation link or click here](https://discord.com/api/oauth2/authorize?client_id=733760070503890994&permissions=8&scope=bot), **why do I ask for admin permission?** because I have a lot of features, so you don't have to bother, you can give me all the permissions, don't worry, I'm very secure, [I give you another link](https://discord.com/api/oauth2/authorize?client_id=733760070503890994&permissions=2147483639&scope=bot) to the one you don't trust or don't trust me, which will be his free choice. ")
+        .addField("<:book3:733698761171271750> Support","Do you need help? If so, I invite you to join my [discord](https://discord.gg/NVBwmFz) which is made for that, or go see my [wiki](https://doc.rox.wtf), it's very well provided!")
         .addField("<a:config:738108401208262738> Configuration","To start using the bot, configure one role as admin and another one as moderator, with the command `!roles` which is only usable by roles admins, and people with `ADMINISTRATOR` permission")
         .setTimestamp()
+        .setColor("#4e64b5")
         .setFooter('Rox • ' + msg.version,client.user.avatarURL())
     channel.send(embed)
-    channel1.send(embed)
+    channel.send(":flag_fr: Pour une version française du message executez la commande `!fr_start`")
 }
