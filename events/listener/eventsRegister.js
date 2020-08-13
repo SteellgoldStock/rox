b716a2d4d8fcfbbc79098757c3713381bc57858d
UnknowG <unknowgdev@gmail.com>
2020-08-14 00:59:53 +0200

    Lang initalizer

--- events/listener/eventsRegister.js
+++ events/listener/eventsRegister.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const {colors, client,fs} = require('./../../rox')
 
+loadLang("database/lang/");
 loadEvent("events/client/");
 loadEvent("events/member/");
 loadEvent("events/guild/");
@@ -10,6 +11,14 @@ function loadEvent(path) {
         files.forEach(file => {
             console.log(colors.yellow("(OK) " + file + " has been loaded"));
             require("../../" + path + file);
+        });
+    });
+}
+
+function loadLang(path) {
+    fs.readdir(path, (err, files) => {
+        files.forEach(file => {
+            console.log(colors.magenta("(OK) " + file + " has been initied"));
         });
     });
 }
