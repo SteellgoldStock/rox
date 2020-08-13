c71b0c8d16d2c5a1b06af23bbac78276d9e5630d
UnknowG <unknowgdev@gmail.com>
2020-08-13 12:08:20 +0200

    Add permission

--- commands/admin/XPSystem.js
+++ commands/admin/XPSystem.js
@@ -5,24 +5,59 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
     if (await msg.Role(message.member, "admin", message, dataServer) === true || message.member.hasPermission('ADMINISTRATOR')) {
 
         if (!args[0]) {
-            return await msg.sendMsg("INVALID_ARGS_CHANNELS", message, dataServer)
+            return await msg.sendMsg("INVALID_ARGS_XP", message, dataServer)
         }
-        const mentionedChannel = message.mentions.channels.first();
+
+        const mentionedRole = message.mentions.roles.first();
 
         switch (args[0]) {
-            case "joinquit":
-                if (!mentionedChannel) {
-                    return await msg.sendMsg("MENTION_CHANNEL", message, dataServer)
+            case "rewards":
+                if(!args[1]){ return await msg.sendMsg("INVALID_ARGS_XP_1", message, dataServer) }
+
+                switch (args[1]){
+                    case "add":
+                        if(!args[2]){ return await msg.sendMsg("INVALID_ARGS_XP_LEVEL_ADD", message, dataServer) }
+
+                        database.query(`SELECT * FROM rewards_level WHERE lvlNum = ${args[2]} AND guildId = ${message.guild.id}`, async function (error, results, fields) {
+                            if (error) {
+                                return false;
+                            } else if (results.length > 0) {
+                                return msg.sendMsg("XP_LEVEL_EXIST", message, dataServer);
+                            } else {
+                                if(!mentionedRole){ return await msg.sendMsg("INVALID_ARGS_XP_LEVEL_ADD", message, dataServer) }
+                                return await update("add",args[2],mentionedRole.id, message.guild.id)
+                            }
+                        });
+                    case "update":
+                        if(!args[2]){ return await msg.sendMsg("INVALID_ARGS_XP_LEVEL_UPDATE", message, dataServer) }
+
+                        database.query(`SELECT * FROM rewards_level WHERE lvlNum = ${args[2]} AND guildId = ${message.guild.id}`, async function (error, results, fields) {
+                            if (error) {
+                                return false;
+                            } else if (results.length > 0) {
+                                if(!mentionedRole){ return await msg.sendMsg("INVALID_ARGS_XP_LEVEL_UPDATE", message, dataServer) }
+                                return await update("update",args[2],mentionedRole.id, message.guild.id)
+                            } else {
+                                return msg.sendMsg("XP_LEVEL_NOT_FOUND", message, dataServer);
+                            }
+                        });
+                    case "remove":
+                        if(!args[2]){ return await msg.sendMsg("INVALID_ARGS_XP_LEVEL_ADD", message, dataServer) }
+
+                        database.query(`SELECT * FROM rewards_level WHERE lvlNum = ${args[2]} AND guildId = ${message.guild.id}`, async function (error, results, fields) {
+                            if (error) {
+                                return false;
+                            } else if (results.length > 0) {
+                                if(!args[2]){ return await msg.sendMsg("INVALID_ARGS_XP_LEVEL_REMOVE", message, dataServer) }
+                                return await update("remove",args[2],mentionedRole.id, message.guild.id)
+                            } else {
+                                return msg.sendMsg("XP_LEVEL_NOT_FOUND", message, dataServer);
+                            }
+                        });
+                    default:
+                        return await msg.sendMsg("INVALID_ARGS_XP_1", message, dataServer)
                 }
-                await update("jq", mentionedChannel.id, message.guild.id);
-                await msg.sendMsg("UPDATED", message, dataServer);
-                break;
-            case "logs":
-                if (!mentionedChannel) {
-                    return await msg.sendMsg("MENTION_CHANNEL", message, dataServer)
-                }
-                await update("logs", mentionedChannel.id, message.guild.id);
-                await msg.sendMsg("UPDATED", message, dataServer);
+            case "status":
                 break;
             default:
                 await msg.sendMsg("INVALID_ARGS_CHANNELS", message, dataServer)
@@ -32,35 +67,28 @@ module.exports.run = async (client, message, args, fs, colors, database, dataSer
     }
 }
 
-async function update(type, id, guildid){
+async function update(type, lvlNum, roleId, guildid) {
     switch (type) {
-        case "jq":
-            var jq = `UPDATE servers SET announceChannel = '${id}' WHERE guildid = '${guildid}'`;
-            database.query(jq, function (err) {
+        case "add":
+            var add = {
+                guildId: guildid,
+                level: lvlNum,
+                roleId: roleId
+            };
+
+            database.query('INSERT INTO rewards_level SET ?', add, function (error, results, fields) {
+                if (error) throw error;
+            });
+            break;
+        case "update":
+            var update = `UPDATE rewards_level SET roleId = '${roleId}' WHERE guildId = '${guildid}'`;
+            database.query(update, function (err) {
                 if (err) throw err;
             });
             break;
-        case "logs":
-            var logs = `UPDATE servers SET logsChannel = '${id}' WHERE guildid = '${guildid}'`;
-            database.query(logs, function (err) {
-                if (err) throw err;
-            });
-            break;
-        case "tickets":
-            var tck = `UPDATE servers SET ticketCat = '${id}' WHERE guildid = '${guildid}'`;
-            database.query(tck, function (err) {
-                if (err) throw err;
-            });
-            break;
-        case "cmds":
-            var cmds = `UPDATE servers SET commandsChannel = '${id}' WHERE guildid = '${guildid}'`;
-            database.query(cmds, function (err) {
-                if (err) throw err;
-            });
-            break;
-        case "is":
-            var is = `UPDATE servers SET interServerChannel = '${id}' WHERE guildid = '${guildid}'`;
-            database.query(is, function (err) {
+        case "remove":
+            var remove = `DELETE FROM rewards_level WHERE guildId = '${guildid}' AND level = '${lvlNum}'`;
+            database.query(remove, function (err) {
                 if (err) throw err;
             });
             break;
@@ -68,5 +96,5 @@ async function update(type, id, guildid){
 }
 
 exports.help = {
-    name: "channels",
+    name: "sysxp",
 }
